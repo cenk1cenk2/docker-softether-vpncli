246df6d2ca3d3d63989960ade5bde8da6e30417c
cenk1cenk2 <cenk1cenk2cenk3@gmail.com>
2019-03-14 13:41:36 +0100

    Initial commit
    Changes to be committed:
    	new file:   .env
    	new file:   .gitignore
    	new file:   README.md
    	new file:   build/dockerfile
    	new file:   build/entrypoint.sh
    	new file:   docker-compose.yml
    	new file:   internalconn.vpn

---
+++ README.md
@@ -0,0 +1,10 @@
+```
+name:         | softether-vpncli-int
+compiler:     | docker-compose + dockerfile
+version:      | v1.3, 20190306
+```
+
+## Description:
+
+Creates a Softether instance to connect to the defined connection in the "./internalconn.vpn". 
+MTU is reduced to 1200 because of this is intended for internal networking.
---
+++ build/entrypoint.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+set -o nounset
+# SOFTETHER START
+echo "Starting Softether Client..."
+## enable ipv4 forwarding
+echo "Enable IPv4 forwarding..."
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+## start vpnclient
+echo "Starting client daemon..."
+setcap 'cap_net_bind_service=+ep' /usr/local/bin/vpnclient
+sleep 2
+/usr/local/bin/vpnclient start
+## create nic and connect to vpn
+echo "Creating Virtual NIC..."
+/usr/bin/expect<<EOD
+set timeout -1
+spawn /usr/local/bin/vpncmd
+expect "Select 1, 2 or 3: "
+send "2\r"
+expect "Hostname of IP Address of Destination: "
+send "\r"
+expect "VPN Client>"
+send "nicdelete\r"
+expect "Virtual Network Adapter Name: "
+send "vpn\r"
+expect "VPN Client>"
+send "niccreate\r"
+expect "Virtual Network Adapter Name: "
+send "vpn\r"
+expect "VPN Client>"
+send "accountdelete internalconn\r"
+expect "VPN Client>"
+send "accountimport\r"
+expect "Import Source File Name: "
+send "/usr/bin/internalconn.vpn\r"
+expect "VPN Client>"
+send "nicsetsetting vpn\r"
+expect "MAC Address to Set: "
+send "$MACADD\r"
+expect "VPN Client>"
+send "nicupgrade vpn\r"
+expect "VPN Client>"
+send "exit\r"
+EOD
+echo "Restart NIC Adapter"
+/usr/local/bin/vpnclient stop
+sleep 2
+/usr/local/bin/vpnclient start
+sleep 3
+echo "Reconnect with desired MAC settings"
+/usr/bin/expect<<EOD
+set timeout -1
+spawn /usr/local/bin/vpncmd
+expect "Select 1, 2 or 3: "
+send "2\r"
+expect "Hostname of IP Address of Destination: "
+send "\r"
+expect "VPN Client>"
+send "accountconnect internalconn\r"
+expect "VPN Client>"
+send "exit\r"
+EOD
+sleep 3
+## Get ip after connection
+echo "\n"
+echo "Renewing ethernet adapter..."
+sysctl -p
+echo "Set MTU to 1200."
+ifconfig vpn_vpn mtu 1200
+sleep 3
+echo "Tailing log files..."
+tail -F /usr/local/libexec/softether/vpnclient/*_log/*.log &
+sleep 2
+ifconfig vpn_vpn down
+sleep 2
+ifconfig vpn_vpn up
+while pgrep vpnclient > /dev/null;:
+do
+	echo "Requesting IP from VPN server..."
+    dhclient vpn_vpn
+    echo "IP address obtained..."
+    ip addr show vpn_vpn
+	sleep 3600
+done
